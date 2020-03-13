0beda4d2ffda0f30f295f82e41af4face0d084ed
Hleb Rubanau <g.rubanau@gmail.com>
2020-03-13 14:41:19 +0300

    remove waiting loop, change roles invocation order, set readonly mode

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,8 +41,8 @@ initialize_playbook() {
 - hosts: localhost
   vars:
     nginx_le_extra_mounts:
-      - "{ \"type\": \"bind\", \"source\": \"/usr/share/wsid\", \"target\": \"/usr/share/wsid\" }"
-      - "{ \"type\": \"bind\", \"source\": \"/var/run/wsid/public\", \"target\": \"/var/run/wsid/public\" }"
+      - "{ \"type\": \"bind\", \"source\": \"/usr/share/wsid\", \"target\": \"/usr/share/wsid\", \"read_only\": true }"
+      - "{ \"type\": \"bind\", \"source\": \"/var/run/wsid/public\", \"target\": \"/var/run/wsid/public\", \"read_only\": true }"
     nginx_le_mode: prod
     nginx_le_brave_mode: true
     nginx_le_compose_version: "3.2" 
